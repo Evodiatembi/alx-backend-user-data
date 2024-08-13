e4abb569418d5db60f6b8fe988f51939144f71cc
Evodiatembi <gmail@evodytembi.com>
2024-08-13 20:50:08 +0000

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,4 +15,27 @@ def _hash_password(password: str) -> str:
     _hash_password.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+def _generate_uuid() -> str:
+    """
+    _generate_uuid.
+    """
+    return str(uuid4())
 
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register_user.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
