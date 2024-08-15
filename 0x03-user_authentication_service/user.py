2384c94877f96d9899dbafcd6ebd5f98d713fe84
Evodiatembi <gmail@evodytembi.com>
2024-08-15 12:25:07 +0000

    user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-""" 
-User module
-
-"""
-
+"""User module"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 Base = declarative_base()
 
 
@@ -25,5 +20,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-
